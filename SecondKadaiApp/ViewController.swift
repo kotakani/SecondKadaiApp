77358cb42ebba8f810692a3818cb49e06462ce84
Kota.Yamada <yamad0710@icloud.com>
2023-03-29 16:31:52 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by yamadakota on 2023/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
